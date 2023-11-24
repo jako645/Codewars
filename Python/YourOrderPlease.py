a3823a3b6535c83aec237e27d52176859f3ba777
Jakub Kokosiński <jakokosinski@student.agh.edu.pl>
2023-11-24 21:05:46 +0100

    Add solution to: Python/YourOrderPlease

---
+++ Python/YourOrderPlease.py
@@ -0,0 +1,16 @@
+import re
+
+def order(sentence):
+    ordered_list = [None] * len(sentence.split())
+
+    for word in sentence.split():
+        no = int(((re.findall('[1-9]', word)))[0])
+
+        ordered_list[no - 1] = word
+    
+    return ' '.join(ordered_list)
+
+
+
+if __name__ == '__main__':
+    print(order(""))
